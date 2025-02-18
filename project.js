9e130c197eba564dbffa48f711cfca18252b04f6
deepak <vijaydeepak942@gmail.com>
2025-02-18 12:35:01 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,102 @@
+//  let header=document.getElementById("head");
+ 
+//  header.textContent="vijay";
+//     header.style.color="red";
+//     header.style.backgroundColor="black";
+//     header.style.fontSize="50px";
+//     header.style.padding="20px";
+// document.write("deepak"); 
+//  a=2;
+//  c=4;
+  
+//  document.write(
+   
+//    a==c? "wrong":"right");
+   //alert("hello")
+  // confirm("hello")
+ // prompt("helsj")
+   
+// let a=20;
+//  let b=40; // dialog box??
+//   if(a>b){
+//     alert("print it")
+//   }else if(a==b&& a!==b){
+//     confirm("print if ssj")
+//   }else{
+//     prompt("  i dont know")
+//   }
+  // var a ="40";
+  // var b  =parseInt(a);
+  // console.log( typeof b);   
+  //   var c=40;
+  //   var e= String(c); // String  S is capital letter??
+  
+  //   console.log(typeof e);
+// switch case ??//
+//  switch( false){
+//   case 10<200 :
+//     answer="right"
+//     break;
+//     case 20==20 :
+//       answer="wrovn"
+//     break;
+//     default:
+//       answer=" i dont kone"
+//       break
+//  }
+//  console.log(answer);
+// scopes??//
+//global scope print  every where ??
+// let f =20; 
+// let b="deepak";
+// function deepak(){
+//   //console.log(b);
+//   //console.log(f)
+// }
+ 
+// deepak();
+ 
+// local scope??
+ 
+// function game(){
+//   let w='cricket';
+//   console.log(w);
+
+//}
+ 
+//game();
+// block scope  let const print only on if condition in side// var can print in and out the value??
+
+// function sports (){
+//   if ( true){
+//     var deepak="cricket";
+//      let v="footba;"
+//       const c="delj"
+//       console.log(c)
+//   }
+  
+// }
+// sports();
+// multi dimental array??//
+//   for loop //
+//  for( let a=0; a<10; a++){
+//   console.log(a+ ","+" hello");
+//  }
+// while loop??
+//  let a=1;
+//   while(a<11){
+//     console.log( a+")"+ a+ a+  a+"deepak")
+//     a++;
+//   };
+//     let  b=233;
+//    while(b>22){
+//     console.log(  "helo");
+//      b--;
+//    }
+      // do-while loop?? 
+      // let d=12;
+      // do{
+      //   console.log( d+")"+"asdfg")
+      //    d--;
+
+      // }while( d>10)
